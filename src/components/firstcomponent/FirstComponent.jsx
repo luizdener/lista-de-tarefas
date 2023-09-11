762cbb8e14fdbb6ee6486d5ba1b4d5c6de305914
Luiz Dener <ldenerjesus@gmail.com>
2023-09-11 06:48:04 -0300

    first commit
    Configurações gerais do projeto, criação da api json server, globals styles

---
+++ src/components/firstcomponent/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import './firstComponent.scss'
+
+const FirstComponent = () => {
+    return (
+        <div className="first-component">
+            <h1>Primeiro Componente</h1>
+        </div>
+    )
+}
+
+export default FirstComponent
